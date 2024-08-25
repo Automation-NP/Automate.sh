3d440cc4c3ad04accc4f52a8d8d19a2434616700
Nikhil <nikhilpraneeth1826@gmail.com>
2024-08-25 18:00:51 +0530

    Test

---
+++ CacheClearupdate.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#Function to view Cache
+Cache() {
+   echo -e "\e[31mViewing Cache\e[0m"
+   free -h
+}
+
+#Cache Clear
+Clear_Cache() {
+    echo -e "/e[34mClearing Cache\e[0m"
+    echo 1 > /proc/sys/vm/drop_caches
+    echo -e "/e[36mCache has been cleared\e[0m"
+}
+
+#Displaying Menu Options
+echo -e "/e[36mExecuting Cache Clear Script\e[0m"
+echo "1. View Cache"
+echo "2. Clear Cache"
+echo "3. Exit"
+
+#Read user Input
+read -p "Choose an option [1-3]: "
+option
+
+case $option in
+
+1)
+   Cache()
+   ;;
+
+2)
+  Clear_Cache()
+  ;;
+
+3) echo "Exiting the Script."
+  ;;
+
+*)
+   echo "Invalid option.
+   Please choose 1,2, or 3."
+   ;;
+
+ esac
