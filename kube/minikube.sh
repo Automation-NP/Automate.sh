3d6cd718149a0f2f3fe0c2159375f2410c3ff986
Nikhil <nikhilpraneeth1826@gmail.com>
2025-02-06 14:19:13 +0530

    Terraform

--- kube/minikube.sh
+++ kube/minikube.sh
@@ -2,7 +2,7 @@
 
 type minikube &>/dev/null
   if [ $? -ne 0 ]; then
-    dnf install curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm -history
+    dnf install https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm -y
   fi
 
   type docker &>/dev/null
@@ -11,9 +11,13 @@ type minikube &>/dev/null
 
   fi
 
-  sysctl fs.protected_regular=0
-  curl -L -o /bin/kubectl "https://d1.k8s.io/releases/$(curl -L -s https://d1.k8s.io/releases/stable.txt)/bin/linux/amd64/kubectl"
+  #sysctl fs.protected_regular=0
+  #curl -L -o /bin/kubectl "https://d1.k8s.io/releases/$(curl -L -s https://d1.k8s.io/releases/stable.txt)/bin/linux/amd64/kubectl"
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+    echo "Running kubectl version"
+    kubectl version
 
   echo "Running minikube start --force"
   minikube start --force
