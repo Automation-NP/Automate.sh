ef3597ddc4df07f0c070faaff9a88e50abf1727a
NikhilPraneeth <nikhilpraneeth1826@gmail.com>
2025-02-23 20:46:37 +0530

    Merge remote-tracking branch 'origin/main'

--- kube/k9install.sh
+++ kube/k9install.sh
@@ -10,7 +10,7 @@ sleep 5s
 # Check if the download was successful
 if [ $? -eq 0 ]; then
     echo "Download successful. Extracting K9s..."
-    tar -xzf /opt/k9s_Linux_amd64.tar.gz
+    tar -xzf /opt/k9s_1/k9s_Linux_amd64.tar.gz
 else
     echo "Download failed"
     exit 1
@@ -19,7 +19,7 @@ fi
 
 # Taking Backup of .bash_profile
 echo "Backing up .bash_profile..."
-cp -pr ~/.bash_profile ~/.bash_profile_$(date +\%Y-\%m-\%d)
+cp -pr ~/.bash_profile ~/.bash_profile$(date +\%Y-\%m-\%d)
 
 # Adding K9s export to .bash_profile
 echo "export K9S=/opt/k9s_1/" >> ~/.bash_profile
@@ -27,7 +27,8 @@ echo "export PATH=\$PATH:/opt/k9s_1" >> ~/.bash_profile
 
 # Reload .bash_profile to apply changes
 echo "Reloading .bash_profile..."
-sleep 5s && source ~/.bash_profile
+source ~/.bash_profile
+sleep 5s
 
 # K9s is ready to use
 echo "K9s is ready to use."
